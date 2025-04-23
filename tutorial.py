33a800c960d81d57b9727a2d4f502a0cf231c439
Scardullo <anthonyscar578@gmail.com>
2025-04-22 23:28:22 -0500

    more_rockheads

--- tutorial.py
+++ tutorial.py
@@ -14,8 +14,10 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
 def flip(sprites):
     return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
 
 def load_sprite_sheets(dir1, dir2, width, height, direction=False):
     path = join("assets", dir1, dir2)
@@ -41,24 +43,6 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
 
     return all_sprites
 
-def load_sprite_3dir(dir1, dir2, dir3, width, height, direction=False):
-    path = join("assets", dir1, dir2, dir3)
-    images = [f for f in listdir(path) if isfile(join(path, f))]
-
-    all_sprites = {}
-
-    for image in images:
-        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
-
-        sprites = []
-        for i in range(sprite_sheet.get_width() // width):
-            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
-            rect = pygame.Rect(i * width, 0, width, height)
-            surface.blit(sprite_sheet, (0, 0), rect)
-            sprites.append(pygame.transform.scale2x(surface))
-            all_sprites[image.replace(".png", "")] = sprites
-
-    return all_sprites
 
 def get_block(size):
     path = join("assets", "Terrain", "Terrain.png")
@@ -68,10 +52,11 @@ def get_block(size):
     surface.blit(image, (0, 0), rect)
     return pygame.transform.scale2x(surface)
 
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
-    SPRITES = load_sprite_sheets("MainCharacters", "NinjaFrog", 32, 32, True)
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
     ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
@@ -164,17 +149,6 @@ class Player(pygame.sprite.Sprite):
     def draw(self, win, offset_x):
         win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
 
-class Checkpoints(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height, name=None):
-        super().__init__()
-        self.rect = pygame.Rect(x, y, width, height)
-        self.image = pygame.Surface((width, height), pygame.SRCALPHA)
-        self.width = width
-        self.height = height
-        self.name = name
-
-    def draw(self, win, offset_x):
-        win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
 
 class Object(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, name=None):
@@ -188,12 +162,14 @@ class Object(pygame.sprite.Sprite):
     def draw(self, win, offset_x):
         win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
 
+
 class Block(Object):
     def __init__(self, x, y, size):
         super().__init__(x, y, size, size)
         block = get_block(size)
         self.image.blit(block, (0, 0))
         self.mask = pygame.mask.from_surface(self.image)
+
 
 class Fire(Object):
     ANIMATION_DELAY = 3
@@ -225,89 +201,6 @@ class Fire(Object):
         if self.animation_count // self.ANIMATION_DELAY > len(sprites):
             self.animation_count = 0
 
-class Flag(Checkpoints):
-    ANIMATION_DELAY = 3
-    SPRITES = load_sprite_3dir("Items", "Checkpoints", "Checkpoint", 64, 64)
-
-    def __init__(self, x, y, width, height):
-        super().__init__(x, y, width, height, "flag")
-        self.flag = load_sprite_3dir("Items", "Checkpoints", "Checkpoint", width, height)
-        self.image = self.flag["Checkpoint (Flag Idle)(64x64)"][0]
-        self.mask = pygame.mask.from_surface(self.image)
-        self.animation_count = 0
-        self.hit = False
-        self.animation_name = "Checkpoint (Flag Idle)(64x64)"
-
-    def on(self):
-        self.animation_name = "Checkpoint (Flag Idle)(64x64)"
-
-    def off(self):
-        self.animation_name = "Checkpoint (Flag Out) (64x64)"
-    
-    def make_hit(self):
-        self.hit = True
-
-    def update_sprite(self):
-        sprite_sheet = "Checkpoint (Flag Idle)(64x64)"
-        if self.hit:
-            sprite_sheet = "Checkpoint (Flag Out) (64x64)"
-
-        sprites = self.SPRITES[sprite_sheet]
-        sprite_index = (self.animation_count //
-                        self.ANIMATION_DELAY) % len(sprites)
-        self.sprite = sprites[sprite_index]
-        self.animation_count += 1
-        self.update()
-        
-    def loop(self):
-        sprites = self.flag[self.animation_name]
-        sprite_index = (self.animation_count //
-                        self.ANIMATION_DELAY) % len(sprites)
-        self.image = sprites[sprite_index]
-        self.animation_count += 1
-
-        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
-        self.mask = pygame.mask.from_surface(self.image)
-
-        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
-            self.animation_count = 0
-        
-        self.update_sprite()
-
-class RockHead(Object):
-    ANIMATION_DELAY = 3
-
-    def __init__(self, x, y, width, height, y_stop):
-        super().__init__(x, y, width, height, "rockhead")
-        self.rockhead = load_sprite_sheets("Traps", "rockhead", width, height)
-        self.image = self.rockhead["Blink (42x42)"][0]
-        self.mask = pygame.mask.from_surface(self.image)
-        self.animation_count = 0
-        self.animation_name = "Blink (42x42)"
-        self.y_vel = 1
-        self.y_stop = y_stop
-
-    def move_down(self):
-        self.rect.y += self.y_vel
-
-    def move_up(self):
-        self.rect.y -= self.y_vel
-
-    def loop(self):
-        sprites = self.rockhead[self.animation_name]
-        sprite_index = (self.animation_count //
-                        self.ANIMATION_DELAY) % len(sprites)
-        self.image = sprites[sprite_index]
-        self.animation_count += 1
-
-        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
-        self.mask = pygame.mask.from_surface(self.image)
-
-        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
-            self.animation_count = 0
-        self.move_down()
-        if self.rect.y >= self.y_stop:
-            self.move_up()
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
@@ -321,18 +214,18 @@ def get_background(name):
 
     return tiles, image
 
-def draw(window, background, bg_image, player, objects, checkpoints, offset_x):
+
+def draw(window, background, bg_image, player, objects, offset_x):
     for tile in background:
         window.blit(bg_image, tile)
 
     for obj in objects:
         obj.draw(window, offset_x)
 
-    for check in checkpoints:
-        check.draw(window, offset_x)
-
     player.draw(window, offset_x)
+
     pygame.display.update()
+
 
 def handle_vertical_collision(player, objects, dy):
     collided_objects = []
@@ -349,6 +242,7 @@ def handle_vertical_collision(player, objects, dy):
 
     return collided_objects
 
+
 def collide(player, objects, dx):
     player.move(dx, 0)
     player.update()
@@ -362,90 +256,43 @@ def collide(player, objects, dx):
     player.update()
     return collided_object
 
-def check_point(player, checkpoints, dx):
-    player.move(dx, 0)
-    player.update()
-    collided_object = None
-    for chk in checkpoints:
-        if pygame.sprite.collide_mask(player, chk):
-            collided_object = chk
-            break
 
-    player.move(-dx, 0)
-    player.update()
-    return collided_object
-
-def handle_move(player, objects, checkpoints, flag):
+def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
     player.x_vel = 0
     collide_left = collide(player, objects, -PLAYER_VEL * 2)
     collide_right = collide(player, objects, PLAYER_VEL * 2)
-    chk_left = check_point(player, checkpoints, -PLAYER_VEL * 2)
-    chk_right = check_point(player, checkpoints, PLAYER_VEL * 2)
 
     if keys[pygame.K_LEFT] and not collide_left:
         player.move_left(PLAYER_VEL)
     if keys[pygame.K_RIGHT] and not collide_right:
         player.move_right(PLAYER_VEL)
-    if keys[pygame.K_LEFT] and not chk_left:
-        player.move_left(PLAYER_VEL)
-    if keys[pygame.K_RIGHT] and not chk_right:
-        player.move_right(PLAYER_VEL)
 
     vertical_collide = handle_vertical_collision(player, objects, player.y_vel)
-    to_check = [collide_left, collide_right, *vertical_collide, chk_left, chk_right]
+    to_check = [collide_left, collide_right, *vertical_collide]
 
     for obj in to_check:
         if obj and obj.name == "fire":
             player.make_hit()
-        elif obj and obj.name == "rockhead":
-            player.make_hit()
-        elif obj and obj.name == "flag":
-            flag.make_hit()
-            flag.off()
+
 
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image = get_background("Purple.png")
+    background, bg_image = get_background("Blue.png")
 
     block_size = 96
 
     player = Player(100, 100, 50, 50)
-
-    rockhead = RockHead(7, block_size * 2, 42, 42, 530)
-    rockhead2 = RockHead(block_size * 3 + 3, 0, 42, 42, 340)
-    rockhead3 = RockHead(block_size * 7 + 4, -200, 42, 42, 150)
-    rockhead4 = RockHead(block_size * 11, -410, 42, 42, 20)
-
-    fire1 = Fire(block_size * 7, HEIGHT - block_size - 64, 16, 32)
-    fire1.on()
-    fire2 = Fire(block_size * 7 - 35, HEIGHT - block_size - 64, 16, 32)
-    fire2.on()
-    fire3 = Fire(block_size * 7 - 70, HEIGHT - block_size - 64, 16, 32)
-    fire3.on()
-    fire4 = Fire(block_size * 7 + 35, HEIGHT - block_size - 64, 16, 32)
-    fire4.on()
-
-    flag = Flag((WIDTH * 2) + (block_size * 5), block_size * 4 - 130, 64, 64)
-    flag.on()
-
+    fire = Fire(170 , HEIGHT - block_size - 64, 16, 32)
+    fire.on()
     floor = [Block(i * block_size, HEIGHT - block_size, block_size)
              for i in range(-WIDTH // block_size, (WIDTH * 2) // block_size)]
-    floor2 = [Block(i * block_size, block_size, block_size)
-             for i in range((block_size * 11) // block_size, (WIDTH * 2) // block_size)]
-    floor3 = [Block(i * block_size, HEIGHT - block_size, block_size)
-              for i in range((block_size * 33) // block_size, WIDTH * 5 // block_size)]
-
-    objects = [*floor, *floor2, *floor3, Block(0, HEIGHT - block_size * 2, block_size),
+    floor2 = [Block(i * block_size, block_size * 2, block_size)
+             for i in range((block_size *11) // block_size, (WIDTH * 2) // block_size)]
+    objects = [*floor, *floor2, Block(0, HEIGHT - block_size * 2, block_size),
                Block(block_size * 3, HEIGHT - block_size * 4, block_size),
-               Block(block_size * 7, HEIGHT - block_size * 6 , block_size), 
-               Block((WIDTH * 2) + (block_size * 4), block_size * 4 , block_size),
-               Block((WIDTH * 2) + (block_size * 5), block_size * 4 , block_size),
-               Block((WIDTH * 2) + (block_size * 6), block_size * 4 , block_size),
-               fire1, fire2, fire3, fire4, rockhead, rockhead2, rockhead3, rockhead4]
-
-    checkpoints = [flag]
+               Block(block_size * 7, HEIGHT - block_size * 6 , block_size), fire]
 
     offset_x = 0
     scroll_area_width = 200
@@ -464,21 +311,9 @@ def main(window):
                     player.jump()
 
         player.loop(FPS)
-
-        fire1.loop()
-        fire2.loop()
-        fire3.loop()
-        fire4.loop()
-        flag.loop()
-
-        rockhead.loop()
-        rockhead2.loop()
-        rockhead3.loop()
-        rockhead4.loop()
-
-        handle_move(player, objects, checkpoints, flag)
-
-        draw(window, background, bg_image, player, objects, checkpoints, offset_x)
+        fire.loop()
+        handle_move(player, objects)
+        draw(window, background, bg_image, player, objects, offset_x)
 
         if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or (
                 (player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
@@ -486,6 +321,7 @@ def main(window):
 
     pygame.quit()
     quit()
+
 
 if __name__ == "__main__":
     main(window)
