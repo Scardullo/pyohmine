de7d9780baadcefe18382630acb52625ba5f9a17
Scardullo <anthonyscar578@gmail.com>
2026-04-22 11:04:33 -0500

    tcp_demo finish/ hypr theme

--- C/tcp_demo.c
+++ C/tcp_demo.c
@@ -49,8 +49,63 @@ void run_server() {
 	buffer[n] = '\0';
 	printf("[SERVER] Received: %s\n", buffer);
     }
+    
+    const char *reply = "Msg from server!";
+    send(conn_fd, reply, strlen(reply), 0);
 
+    close(conn_fd);
+    close(listen_fd);
+}
 
+void run_client() {
+    int sock;
+    struct sockaddr_in_addr;
+    char buffer[BUF_SIZE];
 
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+	perror("socket");
+	exit(1);
+    }
 
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+	perror("connect");
+	exit(1);
+    }
+
+    printf("[CLIENT] Connected to server\n");
+
+    const char *msg = "Msg from client!";
+    send(sock, msg, strlen(msg), 0);
+
+    ssize_t n = recv(sock, buffer, BUF_SIZE - 1, 0);
+    if (n > 0) {
+	buffer[n] = '\0';
+	printf("[CLIENT] Received: %s\n", buffer);
+    }
+
+    close(sock);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+	fprintf(stderr, "Usage: %s [server|client]\n", argv[0]);
+	return 1;
+    }
+
+    if (strmcmp(argv[1], "server") == 0) {
+	run_server();
+    } else if (strcmp(argv[1], "client") == 0) {
+	run_client();
+    } else {
+	fprintf(stderr, "invalid mode\n");
+	return 1;
+    }
+
+    return 0;
 }
