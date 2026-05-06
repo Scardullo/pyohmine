5fb5963f165ef30d19155d9e0cdb1847f5cce30e
Scardullo <anthonyscar578@gmail.com>
2026-05-06 12:18:54 -0500

    candy icons / gruvbox-icons

--- C/students/client.c
+++ C/students/client.c
@@ -14,4 +14,32 @@ int main(){
 
     struct sockaddr_in addr;
     addr.sin_family=AF_INET;
+    addr.sin_port=htons(SERVER_PORT);
+    inet_pton(AF_INET,SERVER_IP,&addr.sin_addr);
+
+    if(connect(sock,(struct sockaddr*)&addr,sizeof(addr))<0){
+	perror("connect"); return 1;
+    }
+
+    char buffer[BUFFER_SIZE];
+    while(1){
+	printf("Command> ");
+	fgets(buffer,sizeof(buffer),stdin);
+	buffer[strcspn(buffer,"\n")]='\0';
+
+	if(send(sock,buffer,strlen(buffer),0)<0){
+	    perror("send"); break;
+	}
+
+	if(strncmp(buffer,"EXIT",4)==0) break;
+
+	int bytes=recv(sock,buffer,sizeof(buffer)-1,0);
+	if(bytes>0){
+	    buffer[bytes]='\0';
+	    printf("%s\n",buffer);
+	}
+    }
+
+    close(sock);
+    return 0;
 }
