3a5140fd7fc35b8a86534f54717fee388abb91a6
Scardullo <anthonyscar578@gmail.com>
2026-05-21 02:18:03 -0500

    keybind cheatsheet

---
+++ dotfiles/config/hypr/scripts/gruvbox.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat <<EOF | rofi -dmenu -i -p "Hyprland Keybinds" \
+-theme ~/.config/rofi/themes/gruvbox.rasi
+
+SUPER + ENTER   Terminal
+SUPER + D       Rofi Launcher
+SUPER + E       Thunar
+SUPER + B       Firefox
+SUPER + C       VS Code
+SUPER + F       Fullscreen
+SUPER + X       Kill Window
+
+SUPER + 1-0     Switch Workspace
+SUPER + SHIFT + 1-0 Move Window To Workspace
+
+SUPER + V       Toggle Floating
+SUPER + S       Toggle Scratchpad
+
+SUPER + PRINT   Screenshot Monitor
+EOF
