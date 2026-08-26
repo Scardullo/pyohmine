b57d1153446657ee5fe4707b2bd770e98247e97d
Scardullo <anthonyscar578@gmail.com>
2026-08-26 07:06:01 -0500

    Wed Aug 26 07:05:48 AM CDT 2026

---
+++ nixos/flakes/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "NixOS configuration for host 'nixos'";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+}
